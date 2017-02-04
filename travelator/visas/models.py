9620693c470057ed200a3ade5db5b8cb2963aa9a
mddemarie <mddemarie@gmail.com>
2017-02-04 10:57:20 +0100

    I created the models in project

--- travelator/visas/models.py
+++ travelator/visas/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+class Entry(models.Model):
+	country_cit = models.Charfield(max_length=50)
+	country_dest = models.Charfield(max_length=50)
+	visa_needed = models.BooleanField(default=None)
 # Create your models here.
