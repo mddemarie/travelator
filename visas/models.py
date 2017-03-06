9804f8822fd541ff3390c87920f91996af1add15
mddemarie <mddemarie@gmail.com>
2017-03-06 11:08:40 +0100

    .gitignore added

--- visas/models.py
+++ visas/models.py
@@ -11,3 +11,4 @@ class Entry(models.Model):
     country_cit = models.CharField(max_length=100)
     country_dest = models.CharField(max_length=100)
     visa_needed = models.BooleanField(default=True)
+#enka42!!
